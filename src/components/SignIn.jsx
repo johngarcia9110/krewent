1da44f79b7a5bede414be7439b487b1e854bafa4
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-15 16:03:06 -0400

    created sign up form component

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class SignIn extends Component {
     render() {
         return(
-            <div>
-            SignIn
+            <div className="form-inline">
+                <h2>Sign In</h2>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="email"/>
+                    <input type="password" className="form-control" placeholder="password"/>
+                    <button className="btn btn-primary">Sign Up</button>
+                </div>
             </div>
         )
     }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../firebase';
 
 class SignUp extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email : '',
+            password : '',
+            error : {
+                message : ''
+            }
+        }
+    }
+
+    signUp(){
+        console.log('this.state', this.state);
+        const { email, password } = this.state;
+        firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+        .catch(error => {
+            console.log('error', error);
+            this.setState({error});
+        })
+    }
+
     render() {
         return(
-            <div>
-            SignUp
+            <div className="form-inline">
+                <h2>Sign Up</h2>
+                <div className="form-group">
+                    <input type="text" 
+                    className="form-control" 
+                    placeholder="email"
+                    onChange={event => this.setState({ email : event.target.value })}
+                    />
+                    <input type="password" 
+                    className="form-control" 
+                    placeholder="password"
+                    onChange={event => this.setState({ password : event.target.value })}
+                    />
+                    <button className="btn btn-primary"
+                    onClick={() => this.signUp()}
+                    >Sign Up</button>
+                </div>
+                <div>{this.state.error.message}</div>
             </div>
         )
     }
