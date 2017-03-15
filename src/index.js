30fd94009fd8449da55e3af49cb2a02eb3df5745
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-15 14:36:35 -0400

    setup react router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
 import App from './components/App'
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 ReactDOM.render(
-    <div>It's alive!!</div>, document.getElementById('root')
+    <Router path="/" history={browserHistory}>
+        <Route path="/app" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+    </Router>, document.getElementById('root')
 )
