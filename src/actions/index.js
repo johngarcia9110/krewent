7040f2655b4ceab6e49698257e067861c37eb7be
johngarcia9110 <johngarcia9110@gmail.com>
2017-03-17 11:51:18 -0400

    reduced tasks in redux store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,17 @@
-import { SIGNED_IN } from '../constants';
+import { SIGNED_IN, SET_TASKS } from '../constants';
 
 export function logUser(email){
     const action = {
         type : SIGNED_IN,
         email
+    }
+    return action;
+}
+
+export function setTasks(tasks){
+    const action = {
+        type : SET_TASKS,
+        tasks
     }
     return action;
 }
